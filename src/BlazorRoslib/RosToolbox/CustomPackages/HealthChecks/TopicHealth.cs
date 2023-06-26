158d7dae8a4380b2728933eb58ee38df0f0ec1bd
DavidBerschauer <43032704+DavidBerschauer@users.noreply.github.com>
2023-06-26 12:18:38 +0200

    Einzelne Cards für jedes Topic

---
+++ src/BlazorRoslib/RosToolbox/CustomPackages/HealthChecks/TopicHealth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace RosToolbox.CustomPackages.HealthChecks;
+
+public class TopicHealth : BlazorRoslib.Core.ROS.Topics.TopicMsgBase
+{
+    public const int TYPE_N_A = 0;
+    public const int TYPE_TIMEOUT = 1;
+    public const int TYPE_WARNING = 2;
+    public const int TYPE_OK = 3;
+    public int type { get; set; }
+    public string? title { get; set; }
+    public float hz { get; set; }
+
+    //public enum Type{
+    //    N_A,
+    //    Timeout,
+    //    Warning,
+    //    Ok
+    //}
+}
+
